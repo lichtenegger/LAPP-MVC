28c4de7f1b24ce5b9a82b6097ccb6a09f1d2e37c
lichtenegger <johanna.lichtenegger@gmail.com>
2015-11-21 16:32:36 +0100

    small fixes

---
+++ BricoMarcheDACH/UI_BricoMarche/Models/BenutzerModelle/RegistrierungModell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UI_BricoMarche.Models.BenutzerModelle
+{
+    public class RegistrierungModell
+    {
+        [Required(ErrorMessage = "Pflichtfeld")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"[\w+\S+\d+]}{5,12}", ErrorMessage = "Passwort muss 5-12 Zeichen lang sein und aus Buchstaben, Ziffern & Sonderzeichen bestehen.")]
+        public string Passwort { get; set; }
+
+        [Required(ErrorMessage = "Pflichtfeld")]
+        [DataType(DataType.Password)]
+        [Compare("Passwort", ErrorMessage = "Passwörter stimmen nicht überein")]
+        public string PasswortWiederholung { get; set; }
+    }
+}
