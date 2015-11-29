88426fc3d73d61f7b2bf4c351b44e69becf1cdde
lichtenegger <johanna.lichtenegger@gmail.com>
2015-11-29 18:33:49 +0100

    Revert "implemented Benutzer.Registrierung & tests"

--- BricoMarcheDACH/BL_BricoMarche/DatenVerwaltung/Benutzer.cs
+++ BricoMarcheDACH/BL_BricoMarche/DatenVerwaltung/Benutzer.cs
@@ -10,87 +10,18 @@ namespace BL_BricoMarche.DatenVerwaltung
 {
     public static class Benutzer
     {
-        #region sindAnmeldeDatenRichtig
         public static bool sindAnmeldeDatenRichtig(string benutzerName, string passwort)
         {
             return Sicherheit.IstPasswortRichtig(benutzerName, passwort);
         }
-        #endregion sindAnmeldeDatenRichtig
-
-        #region registriereBenutzer
-        /// <summary>
-        /// Übergibt ein BenutzerObjekt an die Sicherheitsschicht zum Registrieren
-        /// </summary>
-        /// <param name="benutzerName"></param>
-        /// <param name="passwort"></param>
-        /// <param name="geburtsDatum"></param>
-        /// <param name="vorname"></param>
-        /// <param name="nachname"></param>
-        /// <param name="adresse"></param>
-        /// <param name="ortId"></param>
-        /// <returns>true, false</returns>
-        public static bool registriereBenutzer(string benutzerName, string passwort, DateTime geburtsDatum, string vorname, string nachname, string adresse, int ortId)
-        {
-            BL_BricoMarche.Benutzer neuerBenutzer = new BL_BricoMarche.Benutzer()
-            {
-                Benutzername = benutzerName,
-                Geburtsdatum = geburtsDatum,
-                Vorname = vorname,
-                Nachname = nachname,
-                Adresse = adresse,
-                Ort_ID = ortId,
-                Gruppe_ID = 1
-            };
-            return Sicherheit.Registrierung(neuerBenutzer, passwort);
-        }
-        #endregion registriereBenutzer
 
         #region Sicherheit
-        #region summary
         /// <summary>
         /// Diese Klasse ist nur innerhalb der Benutzerklasse verwendbar & 
         /// soll von außen weder ersichtlich noch angreifbar sein.
         /// </summary>
-        #endregion summary
         private class Sicherheit
         {
-            #region Registrierung
-            /// <summary>
-            /// Speichert neuen Benutzer mit passwort-hash in DB. Gibt retour ob erfolgt oder nicht.
-            /// </summary>
-            /// <param name="neuerBenutzer"></param>
-            /// <param name="passwort"></param>
-            /// <returns>true, false</returns>
-            public static bool Registrierung(BL_BricoMarche.Benutzer neuerBenutzer, string passwort)
-            {
-                bool erfolgt = false;
-                Debug.WriteLine("-- START: REGISTRIERUNG ----------------------------------------------------");
-                Debug.Indent();
-                try
-                {
-                    using (var kontext = new BricoMarcheDBObjekte())
-                    {
-
-                        neuerBenutzer.Passwort = ErmittleHashWert(passwort);
-
-                        kontext.AlleBenutzer.Add(neuerBenutzer);
-                        int anzahlBetroffeneZeilen = kontext.SaveChanges();
-
-                        erfolgt = anzahlBetroffeneZeilen == 1;
-                        Debug.WriteLine("ERFOLG! " + anzahlBetroffeneZeilen + " Benutzer gespeichert!");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine("FEHLER! \n" + ex.Message);
-                    Debugger.Break();
-                }
-                Debug.Unindent();
-                Debug.WriteLine("-- ENDE: REGISTRIERUNG -----------------------------------------------------");
-                return erfolgt;
-            }
-            #endregion Registrierung
-
             #region IstPasswortRichtig
             /// <summary>
             /// Eingegebenes Passwort wird via Funktion ErmittleHashWert verfremdet &
